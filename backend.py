9608a9500ee2237045508228e8e3acea64f35f98
Alex Ilchenko <alec.ilchenko@gmail.com>
2023-12-16 16:36:46 -0600

    Fix all flake8 errors

--- backend.py
+++ backend.py
@@ -2,115 +2,119 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from playsound import playsound
 import time
-import requests
 import json
 
 
-
 class Manager:
-	def __init__(self):
-		#открываем файл с паролем и задаём их
-		with open('login.json', 'r') as f:
-			data = f.readline()
-			login_data = json.loads(data)
-		self.user_name = login_data['user']
-		self.password = login_data['password']
-		
-#переход с первой страницы, где нужно залогиниться
-	def first_page(self):
-		self.driver = webdriver.Chrome()
-		url = 'https://m.xchange-box.com'
-		self.driver.get(url)
-		self.driver.find_element_by_id('login_username').send_keys(self.user_name)
-		password = self.driver.find_element_by_id('login_password')
-		password.send_keys(self.password)
-		password.send_keys(Keys.RETURN)
-		self.driver.implicitly_wait(100)
-		
-#переход со второй страницы по кнопке "Соединение"
-	def second_page(self):
-		self.driver.find_element_by_id('connection_link_open_popup').click()
-		self.driver.implicitly_wait(100)
-		
-#проверяем цены в трейтьем окне
-	def price_check(self):
-		Manager.first_page(self)
-		Manager.second_page(self)
-		price_unit = self.driver.find_element_by_xpath('//*[@id="dialog_connection_content"]/table/tbody/tr[5]/td[2]')
-		price = price_unit.text.replace('$/MiB', '').strip()
-		return price
-		
-#мониторим цену, если она нам не подходит, обновляем страницу и ждём
-	def price_monitor(self):
-		price = Manager.price_check(self)
-		while True:
-			url_page = self.driver.current_url
-			#проверка, если закончилась сессия доступа 
-			if url_page == 'https://m.xchange-box.com/index.php?logout&error=unknown_user#/home.php&ui-state=dialog':
-				self.driver.close()
-				price = Manager.price_check(self)
-			#проверка подходяцей цены
-			if float(price) == 0.45:
-				print('You can connect')
-				self.driver.close()
-				playsound('TVORCHI-Feeling OK.mp3')
-				break
-			#в других случаях обновляем страницу
-			else:
-				time.sleep(5)
-				self.driver.refresh()
-				self.driver.implicitly_wait(100)
-				feed_page = self.driver.current_url
-				if feed_page == 'https://m.xchange-box.com/home.php#/home.php&ui-state=dialog':
-					Manager.second_page(self)
-				price_unit = self.driver.find_element_by_xpath('//*[@id="dialog_connection_content"]/table/tbody/tr[5]/td[2]')
-				price = price_unit.text.replace('$/MiB', '').strip()
-				
-				
-#задать нового пользователя, может быть только один пользователь
-	def set_user(self, user):
-		with open('login.json', 'r') as f:
-			data = f.readline()
-			data = json.loads(data)
-		data['user'] = user
-		json_data = json.dumps(data)
-		with open('login.json', 'w') as f:
-			f.write(json_data)
-			
-#задать новый пароль
-	def set_password(self, password):
-		with open('login.json', 'r') as f:
-			data = f.readline()
-			data = json.loads(data)
-		data['password'] = password
-		json_data = json.dumps(data)
-		with open('login.json', 'w') as f:
-			f.write(json_data)
-		
-#Метод подключает к интернету
-	def connect(self):
-		Manager.first_page(self)
-		Manager.second_page(self)
-		self.driver.find_element_by_xpath('//*[@id="dialog_connection_content"]/form/ul/li[2]/div/div/input').click()
-		self.driver.close()
-		
-#Метод отключает интернет
-	def disconnect(self):
-		Manager.first_page(self)
-		Manager.second_page(self)
-		self.driver.find_element_by_xpath('//*[@id="dialog_connection_content"]/form/ul/li/div/div/input').click()
-		self.driver.implicitly_wait(10)
-		self.driver.close()
-	
-#метод проверяет сколько осталось времени для интренета
-	def check_remaining(self):
-		Manager.first_page(self)
-		Manager.second_page(self)
-		time = self.driver.find_element_by_xpath('//*[@id="remainingTime"]')
-		time = time.text
-		print(f'У тебя осталось времени - {time}')
-		self.driver.close()
-		
-			
-if __name__ == '__main__':
-	pass
+    def __init__(self):
+        self.driver = None
+        with open("login.json", "r") as f:
+            data = f.readline()
+            login_data = json.loads(data)
+        self.user_name = login_data["user"]
+        self.password = login_data["password"]
+
+    def first_page(self):
+        self.driver = webdriver.Chrome()
+        url = "https://m.xchange-box.com"
+        self.driver.get(url)
+        self.driver.find_element_by_id(
+            "login_username"
+        ).send_keys(self.user_name)
+        password = self.driver.find_element_by_id("login_password")
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        self.driver.implicitly_wait(100)
+
+    def second_page(self):
+        self.driver.find_element_by_id("connection_link_open_popup").click()
+        self.driver.implicitly_wait(100)
+
+    def price_check(self):
+        Manager.first_page(self)
+        Manager.second_page(self)
+        price_unit = self.driver.find_element_by_xpath(
+            '//*[@id="dialog_connection_content"]/table/tbody/tr[5]/td[2]'
+        )
+        price = price_unit.text.replace("$/MiB", "").strip()
+        return price
+
+    def price_monitor(self):
+        price = Manager.price_check(self)
+        while True:
+            url_page = self.driver.current_url
+            if (
+                url_page
+                == "https://m.xchange-box.com/index.php?"
+                   "logout&error=unknown_user#/home.php&ui-state=dialog"
+            ):
+                self.driver.close()
+                price = Manager.price_check(self)
+            if float(price) == 0.45:
+                print("You can connect")
+                self.driver.close()
+                playsound("TVORCHI-Feeling OK.mp3")
+                break
+            else:
+                time.sleep(5)
+                self.driver.refresh()
+                self.driver.implicitly_wait(100)
+                feed_page = self.driver.current_url
+                if (
+                    feed_page
+                    == "https://m.xchange-box.com/home.php#/"
+                       "home.php&ui-state=dialog"
+                ):
+                    Manager.second_page(self)
+                price_unit = self.driver.find_element_by_xpath(
+                    '//*[@id="dialog_connection_content"'
+                    ']/table/tbody/tr[5]/td[2]'
+                )
+                price = price_unit.text.replace("$/MiB", "").strip()
+
+    @staticmethod
+    def set_user(user):
+        with open("login.json", "r") as f:
+            data = f.readline()
+            data = json.loads(data)
+        data["user"] = user
+        json_data = json.dumps(data)
+        with open("login.json", "w") as f:
+            f.write(json_data)
+
+    @staticmethod
+    def set_password(password):
+        with open("login.json", "r") as f:
+            data = f.readline()
+            data = json.loads(data)
+        data["password"] = password
+        json_data = json.dumps(data)
+        with open("login.json", "w") as f:
+            f.write(json_data)
+
+    def connect(self):
+        Manager.first_page(self)
+        Manager.second_page(self)
+        self.driver.find_element_by_xpath(
+            '//*[@id="dialog_connection_content"]/form/ul/li[2]/div/div/input'
+        ).click()
+        self.driver.close()
+
+    def disconnect(self):
+        Manager.first_page(self)
+        Manager.second_page(self)
+        self.driver.find_element_by_xpath(
+            '//*[@id="dialog_connection_content"]/form/ul/li/div/div/input'
+        ).click()
+        self.driver.implicitly_wait(10)
+        self.driver.close()
+
+    def check_remaining(self):
+        Manager.first_page(self)
+        Manager.second_page(self)
+        time_limit = self.driver.find_element_by_xpath(
+            '//*[@id="remainingTime"]'
+        )
+        time_limit = time_limit.text
+        print(f"У тебя осталось времени - {time_limit}")
+        self.driver.close()
--- run.py
+++ run.py
@@ -3,45 +3,87 @@ import backend as bc
 
 manager = bc.Manager()
 
-parser = argparse.ArgumentParser(description = 'Программа для работы с судовым интернетом')
-parser.add_argument('-pm', '--price_monitor', action = 'store_true',
-help = 'Write argument -pm or --price_monitor if you want monitor price. If you get value equal 0,45$, sound will be started')
-parser.add_argument('-c', '--connect', action = 'store_true',
-help = 'You will connect to internet')
-parser.add_argument('-sp', '--set_password', type = str,
-help = 'Set your password. It wiil keep in separate file')
-parser.add_argument('-su', '--set_user', type = str,
-help = 'Set your user_name. It will keep in separate file')
-parser.add_argument('-d', '--disconnect', action = 'store_true',
-help = 'It will disconnect internet')
-parser.add_argument('-rt', '--remaining', action = 'store_true',
-help = 'It will check remaining time')
-parser.add_argument('-cp', '--check_price', action = 'store_true',
-help = 'It will print price for internet')
+parser = argparse.ArgumentParser(
+    description="Program for working with ship Internet"
+)
+parser.add_argument(
+    "-pm",
+    "--price_monitor",
+    action="store_true",
+    help="Write argument -pm or --price_monitor if you want monitor price."
+         " If you get value equal 0,45$, sound will be started",
+)
+parser.add_argument(
+    "-c", "--connect", action="store_true", help="You will connect to internet"
+)
+parser.add_argument(
+    "-sp",
+    "--set_password",
+    type=str,
+    help="Set your password. It will keep in separate file",
+)
+parser.add_argument(
+    "-su",
+    "--set_user",
+    type=str,
+    help="Set your user_name. It will keep in separate file",
+)
+parser.add_argument(
+    "-d",
+    "--disconnect",
+    action="store_true",
+    help="It will disconnect internet"
+)
+parser.add_argument(
+    "-rt",
+    "--remaining",
+    action="store_true",
+    help="It will check remaining time"
+)
+parser.add_argument(
+    "-cp",
+    "--check_price",
+    action="store_true",
+    help="It will print price for internet"
+)
 
 
 args = parser.parse_args()
 
-def analize(price_monitor = False, connect = False,
-set_password = None, set_user = None, 
-disconnect = False, remaining = False, 
-check_price = False):
-	if price_monitor == True:
-		manager.price_monitor()
-	if connect == True:
-		manager.connect()
-	if disconnect == True:
-		manager.disconnect()
-	if remaining == True:
-		manager.check_remaining()
-	if set_user != None:
-		manager.set_user(set_user)
-	if set_password !=None:
-		manager.set_password(set_password)
-	if check_price == True:
-		print(manager.price_check())
-		manager.driver.close()
 
-if __name__ == '__main__':
-	analize(args.price_monitor, args.connect, args.set_password, args.set_user, args.disconnect, args.remaining, args.check_price)
-	
+def analyze(
+    price_monitor=False,
+    connect=False,
+    set_password=None,
+    set_user=None,
+    disconnect=False,
+    remaining=False,
+    check_price=False,
+):
+    if price_monitor:
+        manager.price_monitor()
+    if connect:
+        manager.connect()
+    if disconnect:
+        manager.disconnect()
+    if remaining:
+        manager.check_remaining()
+    if set_user is not None:
+        manager.set_user(set_user)
+    if set_password is not None:
+        manager.set_password(set_password)
+    if check_price:
+        print(manager.price_check())
+        manager.driver.close()
+
+
+if __name__ == "__main__":
+    analyze(
+        args.price_monitor,
+        args.connect,
+        args.set_password,
+        args.set_user,
+        args.disconnect,
+        args.remaining,
+        args.check_price,
+    )
